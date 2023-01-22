fbf37c7212edc75ab9649c0806c8bc55f35455e6
Diosx17 <bmelon06800@gmail.com>
2023-01-22 18:07:36 +0100

    Sebastien ta gueule

--- src/main/java/main/Paquets/ModGun.java
+++ src/main/java/main/Paquets/ModGun.java
@@ -11,7 +11,7 @@ public class ModGun {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, gun.MODID);
 
-    public static final RegistryObject<Item> AMMO = ITEMS.register("ammo", () -> new Item(new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> GUN = ITEMS.register("gun", () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> AMMO = ITEMS.register("ammo", () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> GUN = ITEMS.register("gun", () -> new Item(new Item.Properties().stacksTo(1)));
     
 }
